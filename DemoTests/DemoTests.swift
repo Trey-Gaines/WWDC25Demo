d0266f0d1631eec473178ea5897fcf5cd6e4537f
Trey G <trey.gaines@outlook.com>
2025-06-10 16:31:00 -0400

    Initial Commit

---
+++ DemoTests/DemoTests.swift
@@ -0,0 +1,16 @@
+//
+//  DemoTests.swift
+//  DemoTests
+//
+//  Created by Trey Gaines on 6/10/25.
+//
+
+import Testing
+
+struct DemoTests {
+
+    @Test func example() async throws {
+        // Write your test here and use APIs like `#expect(...)` to check expected conditions.
+    }
+
+}
